84018f8365b43d201a17707515c35ce078a872ac
George Kokinis <george.kokinis@gmail.com>
2022-06-27 00:19:31 +0100

    Fix issue with path.

--- BlogBuildScript.sh
+++ BlogBuildScript.sh
@@ -8,4 +8,4 @@ cd ./output
 chown -R nginx:fisher-web .
 find ./ -type f -exec chmod 0570 {} \;
 find ./ -type d -exec chmod 2570 {} \;
-cp -r ./* /srv/www/theeuuk/blog/
+cp -r ./output/* /srv/www/theeuuk/blog/
