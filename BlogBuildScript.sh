4c0803fc77cf85d9d5504a822f766830ffec43b4
George Kokinis <george.kokinis@gmail.com>
2022-08-06 02:52:41 +0100

    Need to fix curl invocation... but just want to actually test what gets downloaded.

--- BlogBuildScript.sh
+++ BlogBuildScript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /var/www/blogbuild/
-
+curl https://nightly.link/fisherthewol/theeuukBlog/workflows/main/main/BlogOutput
 chown -R nginx:fisher-web .
 find ./ -type f -exec chmod 0570 {} \;
 find ./ -type d -exec chmod 2570 {} \;
