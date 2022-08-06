f07b022550d15c0c5b91172465e1ccfa2e304657
George Kokinis <george.kokinis@gmail.com>
2022-08-06 03:01:33 +0100

    Update script to auto-update blog.

--- BlogBuildScript.sh
+++ BlogBuildScript.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 cd /var/www/blogbuild/
-curl https://nightly.link/fisherthewol/theeuukBlog/workflows/main/main/BlogOutput
+curl -LO  https://nightly.link/fisherthewol/theeuukBlog/workflows/main/main/BlogOutput.zip
+unzip BlogOutput.zip
+rm BlogOutput.zip
 chown -R nginx:fisher-web .
 find ./ -type f -exec chmod 0570 {} \;
 find ./ -type d -exec chmod 2570 {} \;
-#cp -r ./output/* /srv/www/theeuuk/blog/
+cp -r ./* /srv/www/theeuuk/blog/
