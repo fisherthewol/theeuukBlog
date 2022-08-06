435489ef68ffcdf78aa23404daa527a08aa0fab1
George Kokinis <george.kokinis@gmail.com>
2022-08-06 02:47:29 +0100

    Begin transition to nightly.link.
    Github actions doesn't have a way to just get "latest" artifact. But you
    can though nightly.link.

--- BlogBuildScript.sh
+++ BlogBuildScript.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 cd /var/www/blogbuild/
-git pull
-pipenv lock
-pipenv sync
-pipenv run pelican --ignore-cache -s publishconf.py content
-cd ./output
+
 chown -R nginx:fisher-web .
 find ./ -type f -exec chmod 0570 {} \;
 find ./ -type d -exec chmod 2570 {} \;
-cp -r ./output/* /srv/www/theeuuk/blog/
+#cp -r ./output/* /srv/www/theeuuk/blog/
