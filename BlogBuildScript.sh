283bc31e4bf6f996ef17f1cc39d82a82eaafdd3b
George Kokinis <george.kokinis@gmail.com>
2023-08-05 23:52:07 +0100

    Remove scripts.

--- BlogBuildScript.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd /var/www/blogbuild/
-curl -LO  https://nightly.link/fisherthewol/theeuukBlog/workflows/main/main/BlogOutput.zip
-unzip BlogOutput.zip
-rm BlogOutput.zip
-chown -R nginx:fisher-web .
-find ./ -type f -exec chmod 0570 {} \;
-find ./* -type d -exec chmod 2570 {} \;
-cp -r ./* /srv/www/theeuuk/blog/
